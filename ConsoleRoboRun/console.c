c89236bbb6bf3fb764f3e9f6d4d49e46fa48e62c
Tim van Herpen <timvherpen@live.nl>
2019-06-03 17:36:16 +0200

    3-7-19

--- ConsoleRoboRun/console.c
+++ ConsoleRoboRun/console.c
@@ -5,6 +5,7 @@
 #include <conio.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include "console.h"
 
 //		Key definitions by ascii number
 #define ESC 27
@@ -16,12 +17,21 @@
 #define KEY_2 50
 #define KEY_3 51
 #define KEY_Q 113
+#define KEY_W 119
+#define KEY_A 97
+#define KEY_S 115
+#define KEY_D 100
+#define KEY_E 101
+
+HANDLE hSerial;
+char comPortNumber = "2";
+char serialPort = "\\\\.\\COM";
 
 void clrscr() {
 	system("cls");
 }
 
-HANDLE serialInit() {
+void serialInit() {
 	//		Init variables
 	extern HANDLE hSerial;
 	DCB dcbSerialParams = { 0 };
@@ -35,7 +45,7 @@ HANDLE serialInit() {
 	timeout.WriteTotalTimeoutMultiplier = 10;
 
 	//		Handle
-	hSerial = CreateFile("\\\\.\\COM8",	// Port name
+	hSerial = CreateFile("\\\\.\\COM2",	// Port name
 		GENERIC_READ | GENERIC_WRITE,	// Read/Write
 		0,								// No Sharing
 		NULL,							// No Security
@@ -56,8 +66,8 @@ HANDLE serialInit() {
 
 	}
 	else {
-		printf("Opening serial port successful\n");
-		menu(hSerial);
+		printf("COM Poort geopend, druk op de any toets om door te gaan.\n");
+		menu();
 	}
 
 
@@ -72,85 +82,106 @@ HANDLE serialInit() {
 	if (!SetCommState(hSerial, &dcbSerialParams)) {
 		//error setting serial port state
 	}
-	return hSerial;
+	//return hSerial;
 }
 
 void serialWrite(char txchar) {
 	BOOL bWriteRC;
 	static DWORD iBytesWritten;
-	bWriteRC = WriteFile(*hSerial, &txchar, 1, &iBytesWritten, NULL);
+	bWriteRC = WriteFile(hSerial, &txchar, 1, &iBytesWritten, NULL);
 }
 
-char serialRead(HANDLE *hSerial) {
-	char rxchar;
+char serialRead(unsigned int bytes) {
+	char rxchar[4];
 	BOOL bReadRC;
 	static DWORD iBytesRead;
-	bReadRC = ReadFile(*hSerial, &rxchar, 1, &iBytesRead, NULL);
+	bReadRC = ReadFile(hSerial, &rxchar, 4 , &iBytesRead, NULL);
 	return rxchar;
 }
 
-void getInput() {
-
+void getComPort() {
+	strcat(serialPort, comPortNumber);
 }
 
 void readBatteryStats() {
 	clrscr();
+	char batteryLevel;
+
+	serialWrite('b');
+	batteryLevel = serialRead(4);
+	batteryLevel = batteryLevel - 4000;
+	batteryLevel = batteryLevel / 10;
+	printf("De batterij is nog %c %c vol", batteryLevel, '%');
+
 }
 
 void coordinateInput() {
-	char coord[3];
+	char coord[10][3];
 	clrscr();
 	printf("Voer hier een coordinaat in waar de robot zich naar toe moet bevinden:\n");
 	fgets(coord, 3, stdin);
 	printf("Je hebt de volgende coordinaat ingegeven:\n");
-	printf(coord);
+	printf("%s\n",coord);
 	
 	
 }
 
 void manualOperation() {
 	clrscr();
+	serialWrite('h');
+	printf("Gebruik WASD of de numpad pijltjestoetsen. Druk op Q om terug te gaan naar het hoofdmenu.\n");
 	char key;
 	char msg[20];
 	do {
 		key = _getch();
+		switch (key) {
+		case KEY_W: serialWrite('w'); break;
+		case KEY_S: serialWrite('f'); break;
+		case KEY_D: serialWrite('d'); break;
+		case KEY_A: serialWrite('a'); break;
+			//case KEY_Q: {serialWrite('q'); active != active; break; }
+		}
 		if (key == 0xE0 || key == 0) {
 			key = 256 + _getch(); //key code has two keys - read the second one
 			switch (key) {
-			case KEY_UP: serialWrite(*hSerial, '^'); break;
-			case KEY_DOWN: serialWrite(*hSerial, 'v'); break;
-			case KEY_LEFT: serialWrite(*hSerial, '<'); break;
-			case KEY_RIGHT: serialWrite(*hSerial, '>'); break;
+			case KEY_UP: serialWrite('w'); break;
+			case KEY_DOWN: serialWrite('f'); break;
+			case KEY_LEFT: serialWrite('d'); break;
+			case KEY_RIGHT: serialWrite('a'); break;
 			}
-			printf("Use your arrow keys to move the robot, or press Q to exit.");
-			putchar('\n');
 			continue;
 		}
 		else {
-			printf("Use your arrow keys to move the robot, or press Q to exit.");
-			putchar('\n');
+			
 		}
 	} while (key != KEY_Q);
 }
 
 void menu() {
 	char key;
-	clrscr();
-	printf("Maak een keuze uit de volgende opties:\n1) Batterij status\n2) Coordinaat invoeren\n3) Handmatige Besturing\nJe kan terugkeren naar dit menu met ESC.\n");
-	do {
-		key = _getch();
-	
+	int done = 1;
+	key = _getch();
+	while (key != ESC) {
+		done = 1;
+		clrscr();
+		printf("Maak een keuze uit de volgende opties:\n1) Batterij status\n2) Coordinaat invoeren\n3) Handmatige Besturing\nJe kan terugkeren naar dit menu met ESC.\n");
+		do {
+			key = _getch();
+
 			switch (key) {
-			case KEY_1: readBatteryStats(); break;
-			case KEY_2: coordinateInput(); break;
-			case KEY_3: manualOperation(); break;
-			
-		}
-	} while (key != ESC);
+			case KEY_1: readBatteryStats(); done = 0; break;
+			case KEY_2: coordinateInput(); done = 0; break;
+			case KEY_3: manualOperation(); done = 0; break;
+			case ESC: done = 0; break;
+			case KEY_S: serialWrite('s);
+			default: printf("Geen geldige keus, probeer opnieuw.\n"); break;
+			}
+		} while (done);
+	}
 }
 
 int main() {
-	HANDLE hSerial = serialInit();
+	serialInit();
 
 	CloseHandle(hSerial);							//Closing the Serial Port
 	return 0;
--- ConsoleRoboRun/console.h
+++ ConsoleRoboRun/console.h
@@ -1,43 +1,10 @@
 #pragma once
 void menu();
 void manualOperation();
-void main();
+int main();
 void readBatteryStats(); 
 void coordinateInput();
 void clrscr();
 void serialWrite();
-char serialRead();
-handle serialInit();
-
-
-
-typedef struct _DCB {
-	DWORD DCBlength;
-	DWORD BaudRate;
-	DWORD fBinary : 1;
-	DWORD fParity : 1;
-	DWORD fOutxCtsFlow : 1;
-	DWORD fOutxDsrFlow : 1;
-	DWORD fDtrControl : 2;
-	DWORD fDsrSensitivity : 1;
-	DWORD fTXContinueOnXoff : 1;
-	DWORD fOutX : 1;
-	DWORD fInX : 1;
-	DWORD fErrorChar : 1;
-	DWORD fNull : 1;
-	DWORD fRtsControl : 2;
-	DWORD fAbortOnError : 1;
-	DWORD fDummy2 : 17;
-	WORD  wReserved;
-	WORD  XonLim;
-	WORD  XoffLim;
-	BYTE  ByteSize;
-	BYTE  Parity;
-	BYTE  StopBits;
-	char  XonChar;
-	char  XoffChar;
-	char  ErrorChar;
-	char  EofChar;
-	char  EvtChar;
-	WORD  wReserved1;
-} DCB, * LPDCB;
+char serialRead(unsigned int bytes);
+void serialInit();
